7851d16d35bc949bcfd5ac6a4444f729c391c3f7
Arthur Ragimov <arthur.ragimov@gmail.com>
2020-05-11 12:07:48 +0300

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:httpapp/parsing_json/json_parsing.dart';
+import 'package:httpapp/parsing_json/json_parsing_map.dart';
 
 void main() => runApp(MaterialApp(
-  home: JsonParsingSimple(),
+  home: JsonParsingMap(),
 ));
--- lib/parsing_json/json_parsing_map.dart
+++ lib/parsing_json/json_parsing_map.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:httpapp/model/post.dart';
 
 class JsonParsingMap extends StatefulWidget {
   @override
@@ -9,9 +10,67 @@ class JsonParsingMap extends StatefulWidget {
 }
 
 class _JsonParsingMapState extends State<JsonParsingMap> {
+  Future<PostList> data;
+
+  @override
+  void initState() {
+    super.initState();
+    Network network = Network("https://jsonplaceholder.typicode.com/posts");
+    data = network.loadPosts();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("PODO"),
+      ),
+      body: Center(
+        child: Container(
+          child: FutureBuilder(
+            future: data,
+            builder: (context, AsyncSnapshot<PostList> snapshot) {
+              List<Post> allPosts;
+              if (snapshot.hasData) {
+                allPosts = snapshot.data.posts;
+
+                return createListView(allPosts, context);
+              } else {
+                return CircularProgressIndicator();
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget createListView(List<Post> data, BuildContext context) {
+    return Container(
+      child: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, int index) {
+          return Column(
+            children: <Widget>[
+              Divider(height: 5.0,),
+              ListTile(
+                title: Text("${data[index].title}"),
+                subtitle: Text("${data[index].body}"),
+                leading: Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                      backgroundColor: Colors.amber,
+                      radius: 23,
+                      child: Text('${data[index].id}'),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
 
@@ -20,15 +79,14 @@ class Network {
 
   Network(this.url);
 
-  Future fetchData() async {
-    print("$url");
-    Response response = await get(Uri.encodeFull(url));
+  Future<PostList> loadPosts() async {
+    final response = await get(Uri.encodeFull(url));
 
     if (response.statusCode == 200) {
-      print(response.body);
-      return json.decode(response.body);
+      // OK
+      return PostList.fromJson(json.decode(response.body));
     } else {
-      print(response.statusCode);
+      throw Exception("Faild to get posts");
     }
   }
 }
